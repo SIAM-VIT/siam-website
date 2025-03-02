415079bcd369bd05b259d79994c981dda5d1ef08
rxshabN <nagwanirishab@gmail.com>
2025-03-02 17:51:20 +0530

    fixed responsiveness issue

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -31,11 +31,9 @@ const LandingPage = () => {
           This website is currently under construction.
         </h2>
         <div className="relative sm:-bottom-5 bottom-0 -rotate-12 bg-yellow-500 border border-white/35 sm:w-[140vw] w-[100vw] h-[35px] opacity-90 z-0">
-          <div className="text-white text-[1.8rem] sm:space-x-20 space-x-28">
+          <div className="text-white text-[1.8rem] sm:space-x-20 space-x-16">
+            <span className="sm:ml-24 ml-5">SIAM-VIT</span>
             <span>SIAM-VIT</span>
-            <span>SIAM-VIT</span>
-            <span className="hidden sm:inline">SIAM-VIT</span>
-            <span className="hidden sm:inline">SIAM-VIT</span>
             <span className="hidden sm:inline">SIAM-VIT</span>
             <span className="hidden sm:inline">SIAM-VIT</span>
             <span className="hidden sm:inline">SIAM-VIT</span>
@@ -45,11 +43,9 @@ const LandingPage = () => {
           </div>
         </div>
         <div className="relative sm:ml-96 bottom-10 sm:bottom-0 rotate-12 bg-yellow-500 border border-white/35 sm:w-[140vw] w-[100vw] h-[35px] opacity-90 z-0">
-          <div className="text-white text-[1.8rem] sm:space-x-20 space-x-24">
+          <div className="text-white text-[1.8rem] sm:space-x-14 space-x-16">
+            <span className="sm:ml-24 ml-5">SIAM-VIT</span>
             <span>SIAM-VIT</span>
-            <span>SIAM-VIT</span>
-            <span className="hidden sm:inline">SIAM-VIT</span>
-            <span className="hidden sm:inline">SIAM-VIT</span>
             <span className="hidden sm:inline">SIAM-VIT</span>
             <span className="hidden sm:inline">SIAM-VIT</span>
             <span className="hidden sm:inline">SIAM-VIT</span>
@@ -119,4 +115,3 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
-
