3847941ab8d4bcfe735acf5b89037796c6ce0e2d
rxshabN <nagwanirishab@gmail.com>
2025-03-09 19:17:24 +0530

    fixed events page, added projects page, added navigation buttons in wrapped page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import LandingPage from "./pages/landing-page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TeamPage from "./pages/team-page";
 import Events from "./pages/Events";
-import Domains from "./pages/domains-page";
-import Wrapped from "./pages/wrapped-page";
+import WrappedPage from "./pages/wrapped-page";
+import DomainsPage from "./pages/domains-page";
 
 function App() {
   return (
@@ -13,8 +13,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/team-page" element={<TeamPage />} />
         <Route path="/events" element={<Events />} />
-        <Route path="/domains" element={<Domains />} />
-        <Route path="/wrapped" element={<Wrapped />} />
+        <Route path="/domains" element={<DomainsPage />} />
+        <Route path="/wrapped" element={<WrappedPage />} />
       </Routes>
     </Router>
   );
--- src/pages/domains-page.jsx
+++ src/pages/domains-page.jsx
@@ -1,43 +1,15 @@
-import React from "react";
+import DomainsPageContent from "../components/domains-page-content";
+import Navbar from "../components/navbar";
 
-const Domains = () => {
+const DomainsPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center min-w-screen min-h-screen bg-gradient-to-l from-[#001B0C] via-[#001B0C] to-black poppins overflow-hidden">
-      {" "}
-      <h1 className="z-30 text-white sm:text-[6rem] text-[2.4rem] text-center">
-        We're coming soon!{" "}
-      </h1>{" "}
-      <h2 className="z-30 text-white sm:text-[1.5rem] mb-32 text-center text-[1rem]">
-        This page is currently under construction.{" "}
-      </h2>{" "}
-      <div className="relative sm:-bottom-5 bottom-0 -rotate-12 bg-yellow-500 border border-white/35 sm:w-[140vw] w-[100vw] h-[35px] opacity-90 z-0">
-        {" "}
-        <div className="text-white text-[1.8rem] sm:space-x-20 space-x-16">
-          <span className="sm:ml-24 ml-5">SIAM-VIT</span>
-          <span>SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>{" "}
-        </div>{" "}
-      </div>{" "}
-      <div className="relative sm:ml-96 bottom-10 sm:bottom-0 rotate-12 bg-yellow-500 border border-white/35 sm:w-[140vw] w-[100vw] h-[35px] opacity-90 z-0">
-        {" "}
-        <div className="text-white text-[1.8rem] sm:space-x-14 space-x-16">
-          <span className="sm:ml-24 ml-5">SIAM-VIT</span>
-          <span>SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>
-          <span className="hidden sm:inline">SIAM-VIT</span>{" "}
-        </div>{" "}
-      </div>{" "}
-    </div>
+    <>
+      <div className="z-[999] sm:fixed absolute w-full">
+        <Navbar />
+      </div>
+      <DomainsPageContent />
+    </>
   );
 };
 
-export default Domains;
+export default DomainsPage;
