60ce62bba8284656d124ebb70bcbbe27751688c3
rxshabN <nagwanirishab@gmail.com>
2025-03-10 17:33:33 +0530

    added about page

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -5,10 +5,11 @@ const hostname = "https://siamvit.com";
 
 const urls = [
   { url: "/", changefreq: "daily", priority: 1 },
-  { url: "/team-page", changefreq: "monthly", priority: 0.9 },
+  { url: "/team", changefreq: "monthly", priority: 0.9 },
   { url: "/events", changefreq: "monthly", priority: 0.9 },
   { url: "/domains", changefreq: "weekly", priority: 0.8 },
   { url: "/wrapped", changefreq: "monthly", priority: 0.8 },
+  { url: "/about", changefreq: "monthly", priority: 0.8 },
 ];
 
 const sitemapStream = new SitemapStream({ hostname });
